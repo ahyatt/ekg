bee9aada0d20f50b2cddd101187177f44f5d38f5
Andrew Hyatt <ahyatt@gmail.com>
2023-01-15 20:48:54 -0500

    Rename ekg-org-roam-import to ekg-org-roam and prepare for packaging
    The intention is to add this to a package repository.

--- ekg-org-roam.el
+++ ekg-org-roam.el
@@ -1,7 +1,29 @@
-;;; ekg-org-roam-import.el --- Function to import all data from org-roam into ekg.  -*- lexical-binding: t -*-
+;;; ekg-org-roam.el --- Importing and exporting between org-roam and ekg  -*- lexical-binding: t -*-
 
+;; Copyright (c) 2023  Free Software Foundation, Inc.
+
+;; Author: Andrew Hyatt <ahyatt@gmail.com>
+;; Homepage: https://github.com/ahyatt/ekg
+;; Package-Requires: ((ekg "0.0") (org-roam "2.0") (emacs "28.1"))
+;; Keywords: outlines, hypermedia
+;; Version: 0.0
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
 ;;; Commentary:
 ;; 
+;; These utilities provide a method for handling the import and (eventually) the
+;; export of data from org-roam into ekg.
 
 (require 'ekg)
 (require 'org-roam)
@@ -131,4 +153,7 @@ JOURNAL-DIR should be relative to PAGES-DIR."
                 (triples-set-type ekg-db (ekg-note-id note) 'org-roam `(:id ,(org-roam-node-id node))))))))))
   (ekg-org-roam-import-logseq org-roam-directory org-roam-dailies-directory))
 
-(provide 'ekg-org-roam-import)
+(provide 'ekg-org-roam)
+
+;;; ekg-org-roam.el ends here
+
