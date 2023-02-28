bfe414a62a90e2052629d36902ca5e3edd2f7827
Andrew Hyatt <ahyatt@gmail.com>
2023-02-27 23:11:27 -0500

    Set version to 0.2.0.

--- ekg.el
+++ ekg.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/ahyatt/ekg
 ;; Package-Requires: ((triples "0.2.5") (emacs "28.1"))
 ;; Keywords: outlines, hypermedia
-;; Version: 0.1.1
+;; Version: 0.2.0
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
 ;; This program is free software; you can redistribute it and/or
