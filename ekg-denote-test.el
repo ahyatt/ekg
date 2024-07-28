c31a6e7d049c0dc0a02065993b21c4ffab95fb04
Andrew Hyatt <ahyatt@gmail.com>
2024-07-28 00:52:46 -0400

    Add exporting to Denote (#161)
    * ekg-denote export changes (#144)
    
    Adds export from ekg to denote.
    
    Authored-by: Jay Rajput
    
    * fix #146 (#148)
    
    Co-authored-by: Jay Rajput
    
    * Note new denote export in docs
    
    * Add denote as an extra dependency
    
    * denote-export: linting and test failures (#155)
    
    * documentation string updates for linting errors.
    
    * documentation string updates for linting errors
    
    * update function names to start with ekg-denote-test
    
    * updates for denote changes
    
    ---------
    
    Co-authored-by: Jay Rajput <jr@Jays-Mac-mini.local>
    
    * Allow emoji tags (#156)
    
    This should fix https://github.com/ahyatt/ekg/issues/149.
    
    * Set version number to 0.6.1 (#157)
    
    * Bump version to 0.7.0
    
    * Rethink version number, let's call the new release 0.6.1
    
    It isn't quite a new release, even though it has some minor functionality / UI
    changes (how tag actions happen).
    
    * Enable the CI for the develop branch (#158)
    
    ---------
    
    Co-authored-by: Jay Rajput <jayrajput@gmail.com>
    Co-authored-by: Jay Rajput <jr@Jays-Mac-mini.local>

---
+++ ekg-denote-test.el
@@ -0,0 +1,187 @@
+;;; ekg-denote-test.el --- Tests for ekg-denote  -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2024  Jay Rajput <jayrajput@gmail.com>
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; These tests should be run and pass before every commit.
+
+;;; Code:
+
+(require 'ert)
+(require 'ekg-test-utils)
+(require 'ekg-denote)
+(require 'cl-lib)
+
+(ert-deftest ekg-denote-test-sublist-keywords ()
+  "Combined length of the sublist keywords is as per the allowed combined length argument."
+  (should (equal '("kw1") (ekg-denote-sublist-keywords '("kw1" "kw2" "kw 3") 4)))
+  (should (equal '("kw1" "kw2") (ekg-denote-sublist-keywords '("kw1" "kw2" "kw 3") 8)))
+  (should (equal '("kw1" "kw2" "kw 3") (ekg-denote-sublist-keywords '("kw1" "kw2" "kw 3") 13))))
+
+(ert-deftest ekg-denote-test-notes-having-duplicate-creation-time ()
+  "Notes with duplicate creation time should error."
+  (let* ((time (time-convert (current-time) 'integer))
+	 (note1 (make-ekg-note :id "ID1" :creation-time time))
+	 (note2 (make-ekg-note :id "ID2" :creation-time time))
+	 (notes (list note1 note2)))
+    (should-error (ekg-denote-assert-notes-have-unique-creation-time notes))))
+
+(ert-deftest ekg-denote-test-notes-having-unique-creation-tiem ()
+  "Notes with unique creation time should not error."
+  (let* ((time (time-convert (current-time) 'integer))
+	 (note1 (make-ekg-note :id "ID1" :creation-time time))
+	 (note2 (make-ekg-note :id "ID2" :creation-time (1+ time)))
+	 (notes (list note1 note2)))
+    (should-not (ekg-denote-assert-notes-have-unique-creation-time notes))))
+
+(ert-deftest ekg-denote-test-notes-missing-creation-time ()
+  "Notes missing creation time should error."
+  (let* ((time (time-convert (current-time) 'integer))
+	 (note1 (make-ekg-note :id "ID1" :creation-time time))
+	 (note2 (make-ekg-note :id "ID2")))
+    (should-error (ekg-denote-assert-notes-have-creation-time notes))))
+
+(ert-deftest ekg-denote-test-notes-not-missing-creation-time ()
+  "Notes having creation time should not error."
+  (let* ((time (time-convert (current-time) 'integer))
+	 (note1 (make-ekg-note :id "ID1" :creation-time time))
+	 (note2 (make-ekg-note :id "ID2" :creation-time (1+ time)))
+	 (notes (list note1 note2)))
+    (should-not (ekg-denote-assert-notes-have-creation-time notes))))
+
+(ert-deftest ekg-denote-test-create ()
+  "Verify creation of `ekg-denote' against given ekg note."
+
+  ; date tag is removed.
+  ; text defaults to ""
+  ; ext defaults to ekg-default-capture-mode
+  ; title defaults to ""
+  ; path uses denote-id and tags
+  ; ekg-id is copied
+  (let* ((time (time-convert (current-time) 'integer))
+	 (denote-directory "/tmp")
+	 (denote-id (format-time-string denote-id-format time))
+	 (ekg-default-capture-mode 'org-mode)
+	 (note (make-ekg-note :id "ID1"
+			      :creation-time time
+			      :tags '("date/20230101" "portfolio" "tag2")))
+	 (denote (make-ekg-denote :id denote-id
+				  :note-id "ID1"
+				  :title ""
+				  :text ""
+				  :path (format "/tmp/%s__portfolio_tag2.org" denote-id )
+				  :kws '("portfolio" "tag2"))))
+    (should (equal denote (ekg-denote-create note))))
+
+  ;; scalar title values shall work
+  (let* ((time (time-convert (current-time) 'integer))
+	 (denote-directory "/tmp")
+	 (denote-id (format-time-string denote-id-format time))
+	 (ekg-default-capture-mode 'org-mode)
+	 (note (make-ekg-note :id "ID1"
+			      :creation-time time
+			      :text "Text"
+			      :properties `(:titled/title "MyTitle")
+			      :tags '("date/20230101" "portfolio")))
+	 (denote (make-ekg-denote :id denote-id
+				  :note-id "ID1"
+				  :title "mytitle"
+				  :text "Text"
+				  :path (format "/tmp/%s--mytitle__portfolio.org" denote-id )
+				  :kws '("portfolio"))))
+    (should (equal denote (ekg-denote-create note))))
+
+  ;; text is copied as it is
+  ;; title is truncated, copied and sluggified
+  (let* ((time (time-convert (current-time) 'integer))
+	 (denote-directory "/tmp")
+	 (denote-id (format-time-string denote-id-format time))
+	 (ekg-default-capture-mode 'org-mode)
+	 (ekg-denote-export-title-max-len 10)
+	 (note (make-ekg-note :id "ID1"
+			      :creation-time time
+			      :text "Text"
+			      :properties `(:titled/title ,(list "Title 123456789"))
+			      :tags '("date/20230101" "portfolio")))
+	 (denote (make-ekg-denote :id denote-id
+				  :note-id "ID1"
+				  :title "title-1234"
+				  :text "Text"
+				  :path (format "/tmp/%s--title-1234__portfolio.org" denote-id )
+				  :kws '("portfolio"))))
+    (should (equal denote (ekg-denote-create note)))))
+
+
+(defun ekg-test--matching-denote (regexp)
+  "Get denote file name containing REGEXP.
+Enforces single match."
+  (let ((files (seq-filter (lambda (x) (string-match-p regexp x))
+			   (denote-directory-files nil nil :text-only))))
+    (when files (car files))))
+
+(defun ekg-test--denote-text (file)
+  "Get text from denote FILE."
+  (with-temp-buffer
+    (insert-file-contents file)
+    (buffer-string)))
+
+(defun ekg-test--creation-time-from-denote-file (file)
+  "Return creation time for the denote FILE."
+  (time-convert
+   (encode-time
+    (parse-time-string
+     (denote-retrieve-filename-identifier file)))
+   'integer))
+
+(defun ekg-test--denote-ekg (file)
+  "Return ekg note for given denote FILE."
+  (let* ((creation-time (ekg-test--creation-time-from-denote-file file))
+	 (triples (triples-db-select-pred-op ekg-db :time-tracked/creation-time '= creation-time)))
+    (should (length= triples 1))
+    (ekg-get-note-with-id (car (car triples)))))
+
+(ekg-deftest ekg-denote-test-export ()
+  "Verify export."
+  (let ((denote-directory (make-temp-file "denote" t)))
+    ;; ekg note creations are exported
+    (ekg-save-note (ekg-note-create :text "text1" :mode 'org-mode :tags '("portfolio")))
+    (sleep-for 1)
+    (ekg-denote-export)
+    (setq denote-file (ekg-test--matching-denote "__portfolio"))
+    (should (equal "text1" (ekg-test--denote-text denote-file)))
+    ;; ekg note updates are exported
+    (setq note (ekg-test--denote-ekg denote-file))
+    (setf (ekg-note-text note) "text2")
+    (ekg-save-note note)
+    (sleep-for 1)
+    (ekg-denote-export)
+    (setq denote-file (ekg-test--matching-denote "__portfolio"))
+    (should (equal "text2" (ekg-test--denote-text denote-file)))
+    ;; ekg note tag update cause denote file rename
+    (setq note (ekg-test--denote-ekg denote-file))
+    (setf (ekg-note-tags note) '("updatedtag"))
+    (ekg-save-note note)
+    (sleep-for 1)
+    (ekg-denote-export)
+    (should (ekg-test--matching-denote "__updatedtag"))
+    (should-not (ekg-test--matching-denote "__portfolio"))))
+
+(ekg-deftest ekg-denote-test-last-export ()
+  "Verify last export time updates."
+  (should (= 0 (ekg-denote-get-last-export)))
+  (ekg-denote-set-last-export 123)
+  (should (= 123 (ekg-denote-get-last-export))))
---
+++ ekg-denote.el
@@ -0,0 +1,218 @@
+;;; ekg-denote.el --- ekg and denote integration -*- lexical-binding: t -*-
+
+;; Copyright (c) 2024  Jay Rajput <jayrajput@gmail.com>
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; This package provides integration between ekg and denote.
+;;
+;; During export, for each ekg note, a denote file is created.  Denote
+;; does not allow creation time for two notes within a second whereas
+;; ekg has no such restriction, so it is necessary to ensure that each
+;; ekg note has a unique creation time for export.  Additionally,
+;; denote embeds the title and tags in the filename, which is limited
+;; based on the underlying operating system.  The titles and tags of
+;; ekg notes are trimmed to a configurable length before export.  Ekg
+;; notes can have creation time within a second when trying to bulk
+;; import org-roam files to ekg.
+;;
+
+
+(require 'ekg)
+(require 'denote)
+(require 'triples)
+(require 'seq)
+
+;;; Code:
+
+(defcustom ekg-denote-export-title-max-len 50
+  "Maximum length of the title to trim to during export."
+  :type 'integer
+  :group 'ekg-denote-export)
+
+(defcustom ekg-denote-export-combined-keywords-len 150
+  "Maximum length of the combined keywords used for trimming kws when converting tags to keywords during export."
+  :type 'integer
+  :group 'ekg-denote-export)
+
+(defcustom ekg-denote-export-add-front-matter nil
+  "Whether front-matter is added or not to denotes during export."
+  :type 'boolean
+  :group 'ekg-denote-export)
+
+(defcustom ekg-denote-export-backup-on-conflict t
+  "Whether to backup denotes using `backup-buffer' on conflict during export.
+You can choose not to backup in case denotes are already backed up using git or in other usecases."
+  :type 'boolean
+  :group 'ekg-denote-export)
+
+(defun ekg-denote-connect ()
+  "Connect to ekg and ensure denote schema is set up."
+  (ekg-connect)
+  (ekg-denote-add-schema))
+
+(defun ekg-denote-add-schema ()
+  "Add the denote schema to the ekg database."
+  (triples-add-schema ekg-db 'denote
+		      '(last-export :base/unique t :base/type integer)
+		      '(last-import :base/unique t :base/type integer)))
+
+(defun ekg-denote-get-last-export ()
+  "Get the last export time."
+  (or (plist-get
+       (triples-get-type ekg-db 'denote 'denote)
+       :last-export) 0))
+
+(defun ekg-denote-set-last-export (time)
+  "Set the last export time to TIME."
+  (let ((plist (triples-get-type ekg-db 'denote 'denote)))
+    (apply #'triples-set-type ekg-db 'denote 'denote
+	   (plist-put plist :last-export (floor (float-time time))))))
+
+(defun ekg-denote--triples-get-rows-modified-since (time)
+  "Return rows modified since TIME."
+  (let ((pred (if (= 0 time) :time-tracked/creation-time :time-tracked/modified-time)))
+    (triples-db-select-pred-op ekg-db pred '>= time)))
+
+(defun ekg-denote--notes-modified-since (time)
+  "Return notes modified since TIME."
+  (let* ((rows (ekg-denote--triples-get-rows-modified-since time))
+	 (ids (mapcar #'car rows))
+	 (notes (mapcar #'ekg-get-note-with-id ids)))
+    (remove nil notes)))
+
+(defun ekg-denote-sublist-keywords (kws combined-length)
+  "Return the sublist for the given KWS list.
+The length of combined KWS is not more than the given
+COMBINED-LENGTH."
+  (if (length< (denote-keywords-combine kws) combined-length) kws
+    (ekg-denote-sublist-keywords (butlast kws) combined-length)))
+
+(cl-defstruct ekg-denote
+  "Representation of denote file."
+  id note-id text kws title path)
+
+(defun ekg-denote-create (note)
+  "Create a new `ekg-denote' from given NOTE."
+  (let* ((id (format-time-string denote-id-format (ekg-note-creation-time note)))
+	 (note-id (ekg-note-id note))
+	 (text (or (ekg-note-text note) ""))
+	 (ext (if (eq ekg-capture-default-mode 'org-mode) ".org" ".md"))
+	 ;; remove date tag as denote uses date in ID.
+	 (tags (seq-filter (lambda (tag)
+			     (not (string-prefix-p "date/" tag))) (ekg-note-tags note)))
+	 (kws (ekg-denote-sublist-keywords
+	       (denote-sluggify-keywords tags) ekg-denote-export-combined-keywords-len))
+	 (ekg-title (plist-get (ekg-note-properties note) :titled/title))
+	 (ekg-title (if (listp ekg-title) (car ekg-title) ekg-title))
+	 (ekg-title (or ekg-title ""))
+	 (title (string-limit (denote-sluggify 'title ekg-title) ekg-denote-export-title-max-len))
+	 (signature-slug "")
+	 (path (denote-format-file-name (file-name-as-directory denote-directory) id kws title ext signature-slug)))
+    (make-ekg-denote :id id
+		     :note-id note-id
+		     :text text
+		     :kws kws
+		     :title title
+		     :path path)))
+
+(defun ekg-denote--backup (denote)
+  "Backup given DENOTE."
+  (when ekg-denote-export-backup-on-conflict
+    (message "ekg-denote: Taking backup of %s" (ekg-denote-path denote))
+    (with-current-buffer (find-file-noselect (ekg-denote-path denote))
+      (let ((make-backup-files t)
+	    (backup-by-copying t)
+	    (backup-inhibited nil))
+	(backup-buffer)))))
+
+(defun ekg-denote--rename-if-path-changed (denote)
+  "Rename given DENOTE if path has changed.
+Path can change due to title or tag changes."
+  (let* ((id (ekg-denote-id denote))
+	 (path (ekg-denote-path denote))
+	 (existing-path (denote-get-path-by-id id)))
+    (when (and existing-path (not (string= existing-path path)))
+      (denote-rename-file-and-buffer existing-path path))))
+
+(defun ekg-denote--text-save (denote)
+  "Save the text from given DENOTE to the disk.
+Optionally add front-matter."
+  (let ((path (ekg-denote-path denote))
+	(text (ekg-denote-text denote))
+	(title (ekg-denote-title denote))
+	(kws (ekg-denote-kws denote)))
+    (with-temp-file path (insert text))
+    (when ekg-denote-export-add-front-matter
+      (denote-add-front-matter path title kws))))
+
+(defun ekg-denote--modified-time-from-file (denote)
+  "Return modified time for the DENOTE."
+  (let ((path (ekg-denote-path denote)))
+    (when (file-exists-p path)
+      (time-convert
+       (file-attribute-modification-time
+	(file-attributes path))
+       'integer))))
+
+(defun ekg-denote--note-print (note)
+  "Return string representation of NOTE for printing."
+  (format "Note ID: %s, Modified: %s, Created: %s, Tags: %s, Title: %s, Text: %s"
+	  (ekg-note-id note)
+	  (ekg-note-modified-time note)
+	  (ekg-note-creation-time note)
+	  (ekg-note-tags note)
+	  (plist-get (ekg-note-properties note) :titled/title)
+	  (truncate-string-to-width (or (ekg-note-text note) "") 100 nil nil "...")))
+
+(defun ekg-denote-assert-notes-have-creation-time (notes)
+  "Raise error if NOTES are missing creation-time.
+Denote uses creation-time as ID."
+  (cl-loop for note in notes do
+	   (when (not (ekg-note-creation-time note))
+	     (error (format "ekg-denote: note missing creation time: %s" (ekg-denote--note-print note))))))
+
+(defun ekg-denote-assert-notes-have-unique-creation-time (notes)
+  "Raise error if NOTES are using duplicate creation-time.
+Denote uses creation-time as ID and assume it to be unique."
+  (let ((creation-times (mapcar #'ekg-note-creation-time notes)))
+    (when (not (equal creation-times (seq-uniq creation-times)))
+      (error "ekg-denote: Notes using same creation time"))))
+
+(defun ekg-denote-export ()
+  "Export the current ekg database to denote."
+  (interactive)
+  (ekg-denote-connect)
+  (let* ((last-export-time (ekg-denote-get-last-export))
+	 (start-time (current-time))
+	 (notes (ekg-denote--notes-modified-since last-export-time)))
+    (and (ekg-denote-assert-notes-have-creation-time notes)
+	 (ekg-denote-assert-notes-have-unique-creation-time notes))
+    (message "ekg-denote-export: exporting notes modified since epoch: %s, date-time: %s"
+	     last-export-time
+	     (format-time-string "%Y%m%dT%H%M%S" last-export-time))
+    (cl-loop for note in notes do
+	     (message "ekg-denote-export: exporting %s." (ekg-denote--note-print note))
+	     (let* ((denote (ekg-denote-create note))
+		    (modified-at (ekg-denote--modified-time-from-file denote)))
+	       (when (and modified-at (time-less-p last-export-time modified-at))
+		 (ekg-denote--backup denote))
+	       (ekg-denote--rename-if-path-changed denote)
+	       (ekg-denote--text-save denote)))
+    (message "ekg-denote-export: completed.")
+    (ekg-denote-set-last-export start-time)))
+
+(provide 'ekg-denote)
+;;; ekg-denote.el ends here.
