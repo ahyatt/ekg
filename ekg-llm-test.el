ahyatt/ekg

commit 354d937fbfcaead34a0b0b30bfa82c0ee48ca95c

Improve the LLM json representation of past notes for LLM context (#172)
Add ID to the JSON, if interesting.

Add any properties that have a label defined.

Remove some duplication of the properties.

Sort to get stable JSON (really just needed for tests).

Add a test for the JSONification.

doc/ekg.org
@@ -77,6 +77,7 @@ triples library is already installed.
 - Fix issue in trying to get a single-valued item to show up in the metadata line.
 - Fix issue with tag completions triggering incorrectly if they contained spaces.
 - Fix error when displaying unknown schema.
+- Improve JSONification of notes when sending context to LLMs.
 ** Version 0.6.2
 - Add export to [[https://github.com/protesilaos/denote][denote]] (by [[https://github.com/jayrajput][Jay Rajput]])
 ** Version 0.6.1

doc/ekg.texi
@@ -230,6 +230,8 @@ Fix issue in trying to get a single-valued item to show up in the metadata line.
 Fix issue with tag completions triggering incorrectly if they contained spaces.
 @item
 Fix error when displaying unknown schema.
+@item
+Improve JSONification of notes when sending context to LLMs.
 @end itemize
 
 @node Version 062

ekg-llm.el
@@ -34,6 +34,7 @@
 (require 'llm)
 (require 'llm-prompt)
 (require 'json)
+(require 'map)
 (require 'org nil t)
 
 ;;; Code:
@@ -241,10 +242,18 @@ structs."
   (let ((result `((tags . ,(ekg-note-tags note))
                   (created . ,(ekg-llm-format-time (ekg-note-creation-time note)))
                   (modified . ,(ekg-llm-format-time (ekg-note-modified-time note)))
-                  (text . ,(substring-no-properties (ekg-display-note-text note)))))
-        (titles (plist-get (ekg-note-properties note) :titled/title))
+                  (text . ,(substring-no-properties (substring-no-properties
+                                                     (ekg-note-text note))))))
         (json-encoding-pretty-print t))
-    (when titles (push (cons 'titles titles) result))
+    (when (ekg-should-show-id-p note)
+      (push (cons "id" (ekg-note-id note)) result))
+    (map-do
+     (lambda (prop value)
+       (when-let ((label (assoc-default prop ekg-metadata-labels)))
+         (push (cons (downcase label) value) result)))
+     (ekg-note-properties note))
+    ;; Sort the result so JSON is deterministic and we can test it.
+    (sort result (lambda (a b) (string< (car a) (car b))))
     (json-encode result)))
 
 (defun ekg-llm-make-any-tag-generator (tags)

ekg-llm-test.el
@@ -44,6 +44,28 @@
       (should (equal "BEGIN\nvery different text\nEND\n"
                      (substring-no-properties (buffer-string)))))))
 
+(ert-deftest ekg-llm-test-note-to-text ()
+  (let* ((time (current-time))
+         (time-str (format-time-string "%Y-%m-%dT%H:%M:%S" time))
+         (json-encoding-pretty-print t))
+    (should (equal
+             (json-encode
+              (sort
+               `(("tags" . ["tag1" "tag2"])
+                 ("created" . ,time-str)
+                 ("modified" . ,time-str)
+                 ("title" . ["Title"])
+                 ("text" . "Content")
+                 ("id" . "http://example.com/1"))
+               (lambda (a b) (string< (car a) (car b)))))
+             (ekg-llm-note-to-text
+              (make-ekg-note :id "http://example.com/1"
+                             :properties '(:titled/title ("Title"))
+                             :text "Content"
+                             :creation-time time
+                             :modified-time time
+                             :tags '("tag1" "tag2")))))))
+
 (provide 'ekg-llm-test)
 
 ;;; ekg-llm-test.el ends here

commit 4dbea886a472f1e829a56279f8f7ff8aa42df329

Fix LLMs not expanding inlines, broken in commit #172 (#180)
Also add a check to the test to keep this problem from re-occurring.

doc/ekg.org
@@ -76,6 +76,7 @@ triples library is already installed.
 ** Version 0.6.4
 - Fix display of extended structured data in notes.
 - Add contrib directory and =ekg-email= as a contribution to import emails to notes in a structured way.
+- Fix inlines not expanding in the contextual notes in =ekg-llm=.
 ** Version 0.6.3
 - Fix issue in trying to get a single-valued item to show up in the metadata line.
 - Remove the never-used named, email, and person schema.

doc/ekg.texi
@@ -231,6 +231,8 @@ triples library is already installed.
 Fix display of extended structured data in notes.
 @item
 Add contrib directory and @samp{ekg-email} as a contribution to import emails to notes in a structured way.
+@item
+Fix inlines not expanding in the contextual notes in @samp{ekg-llm}.
 @end itemize
 
 @node Version 063

ekg-llm.el
@@ -243,7 +243,7 @@ structs."
                   (created . ,(ekg-llm-format-time (ekg-note-creation-time note)))
                   (modified . ,(ekg-llm-format-time (ekg-note-modified-time note)))
                   (text . ,(substring-no-properties (substring-no-properties
-                                                     (ekg-note-text note))))))
+                                                     (ekg-display-note-text note))))))
         (json-encoding-pretty-print t))
     (when (ekg-should-show-id-p note)
       (push (cons "id" (ekg-note-id note)) result))

ekg-llm-test.el
@@ -48,23 +48,29 @@
   (let* ((time (current-time))
          (time-str (format-time-string "%Y-%m-%dT%H:%M:%S" time))
          (json-encoding-pretty-print t))
-    (should (equal
-             (json-encode
-              (sort
-               `(("tags" . ["tag1" "tag2"])
-                 ("created" . ,time-str)
-                 ("modified" . ,time-str)
-                 ("title" . ["Title"])
-                 ("text" . "Content")
-                 ("id" . "http://example.com/1"))
-               (lambda (a b) (string< (car a) (car b)))))
-             (ekg-llm-note-to-text
-              (make-ekg-note :id "http://example.com/1"
-                             :properties '(:titled/title ("Title"))
-                             :text "Content"
-                             :creation-time time
-                             :modified-time time
-                             :tags '("tag1" "tag2")))))))
+    (cl-letf (((symbol-function 'ekg-inline-command-const-inline)
+               (lambda (&rest _) "inline")))
+      (should (equal
+               (json-encode
+                (sort
+                 `(("tags" . ["tag1" "tag2"])
+                   ("created" . ,time-str)
+                   ("modified" . ,time-str)
+                   ("title" . ["Title"])
+                   ("text" . "Contentinline\n")
+                   ("id" . "http://example.com/1"))
+                 (lambda (a b) (string< (car a) (car b)))))
+               (ekg-llm-note-to-text
+                (make-ekg-note :id "http://example.com/1"
+                               :properties '(:titled/title ("Title"))
+                               :text "Content"
+                               :creation-time time
+                               :modified-time time
+                               :tags '("tag1" "tag2")
+                               :inlines
+                               (list (make-ekg-inline :pos 7
+                                                      :command '(const-inline)
+                                                      :type 'command)))))))))
 
 (provide 'ekg-llm-test)
 