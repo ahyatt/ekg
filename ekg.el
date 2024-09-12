68359d4d4fd0d10a9089851e6daa71397745e69f
S Pearman <monkpearman@icloud.com>
2024-09-12 12:24:52 -0400

    update `ekg-tags-complete' for `ekg-tags-completion-function'

--- ekg.el
+++ ekg.el
@@ -1815,20 +1815,21 @@ tags)."
 
 (cl-defun ekg-tags-complete (&key prompt collection predicate require-match 
                                   initial-input hist def inherit-input-method)
-  "Select an ekg tag from the current `ekg-db' as if by `completing-read'.
+  "Select an ekg tag from the current `ekg-db'.
+Tag completion performed according to specified `ekg-tags-completion-function'.
 Keyword arguments PROMPT, COLLECTION, PREDICATE, REQUIRE-MATCH, INITIAL-INPUT,
 HIST, DEF, and INHERIT-INPUT-METHOD are as per `completing-read', which see.
 See also `ekg-tags-complete-doc', `ekg-tags'."
   (interactive)
-  (completing-read
-   (or prompt "Select an ekg tag: ")
-   (or collection (ekg-tags))
-   predicate
-   require-match
-   initial-input
-   hist
-   def
-   inherit-input-method))
+  (apply ekg-tags-completion-function
+         (or prompt "Select an ekg tag: ")
+         (or collection (ekg-tags))
+         predicate
+         require-match
+         initial-input
+         hist
+         def
+         inherit-input-method))
 
 (defun ekg-tags-complete-doc ()
   "Select a doc/tag from the current `ekg-db' as if by `completing-read'.
